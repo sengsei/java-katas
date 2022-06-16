55d6a5e9cf19ddd1d2f99ede2cc11ae8ececf4de
sengsei <sebastian.sengbusch@stud.th-luebeck.de>
2022-06-16 19:00:52 +0200

    first idea

---
+++ src/test/java/kyu7/LengthAndTwoValuesTest.java
@@ -0,0 +1,32 @@
+package kyu7;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LengthAndTwoValuesTest {
+    @Test
+    void smallArray() {
+        assertEquals(new String[]{"true", "false", "true", "false", "true"},
+                5, "true", "false");
+    }
+
+    @Test
+    void largerArray() {
+        assertEquals(new String[]{"blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"},
+                20, "blue", "red");
+    }
+
+    @Test
+    void withZero() {
+        assertEquals(new String[0], 0, "lemons", "apples");
+    }
+
+    private void assertEquals(String[] expected, int n, String firstValue, String secondValue) {
+        String[] actual = LengthAndTwoValues.alternate(n, firstValue, secondValue);
+        assertArrayEquals(expected, actual, String.format("For input (%d, \"%s\", \"%s\")%nExpected: %s%nActual: %s%n", n, firstValue, secondValue, Arrays.toString(expected), Arrays.toString(actual)));
+    }
+
+}
